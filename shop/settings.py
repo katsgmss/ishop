5e19d9596be297e164f8d6c4cac00fc864bcad81
Loukas Kiniakos <detegere@Loukass-MacBook-Pro.local>
2024-12-15 13:08:24 +0200

    Commented default database to use the postgres one for testing purposes

--- shop/settings.py
+++ shop/settings.py
@@ -74,18 +74,18 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# import dj_database_url
-
-# DATABASES = {
-#     'default': dj_database_url.config(default='postgres://localhost:5432/mydb', conn_max_age=600)
-# }
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://localhost:5432/mydb', conn_max_age=600)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # import os
